1a29e560df1d1ced21d1556c472178318ba38ea7
AtillaAkbulut <atiakbulut1@gmail.com>
2026-07-11 02:55:48 +0300

    example: add NPC dialogue memory

--- demo_scene.gd
+++ demo_scene.gd
@@ -1,8 +1,13 @@
 extends Node2D
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	_setup_dialogue_providers()
+	_setup_demo_state()
+	_connect_dialogue_events()
+
+
+func _setup_dialogue_providers() -> void:
 	DialogueVariableResolver.set_provider(
 		Callable(GameState, "get_variable")
 	)
@@ -10,27 +15,44 @@ func _ready() -> void:
 	DialogueConditionResolver.set_provider(
 		Callable(GameState, "has_flag")
 	)
-	
+
+
+func _setup_demo_state() -> void:
+	# Dialogue variables
 	GameState.set_variable("player_name", "Atilla")
 	GameState.set_variable("gold", 120)
 	
+	# NPC memory
+	GameState.set_flag("talked_to_old_man", false)
+	
+	# Quest example
 	GameState.set_flag("quest_1_active", false)
 	GameState.set_flag("has_old_key", false)
 	GameState.set_flag("key_already_given", true)
-	
-	DialogueManager.dialogue_event_triggered.connect(
+
+
+func _connect_dialogue_events() -> void:
+	if not DialogueManager.dialogue_event_triggered.is_connected(
 		_on_dialogue_event_triggered
-	)
-	
-	
+	):
+		DialogueManager.dialogue_event_triggered.connect(
+			_on_dialogue_event_triggered
+		)
+
 
 func _on_dialogue_event_triggered(event_id: String) -> void:
 	print("Dialogue Event Triggered: ", event_id)
-
+	
 	match event_id:
+		"old_man_met_player":
+			GameState.set_flag("talked_to_old_man", true)
+		
 		"old_key_given":
 			GameState.set_flag("has_old_key", false)
 			GameState.set_flag("key_already_given", true)
-
+		
 		"quest_1_completed":
 			GameState.set_flag("quest_1_active", false)
+		
+		_:
+			print("Unhandled dialogue event: ", event_id)
